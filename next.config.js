3a46b03c7595318e260f7283ce8f62d65698772f
erezrokah <erezrokah@users.noreply.github.com>
2021-02-23 12:32:01 +0100

    chore: publish markdown files

--- next.config.js
+++ next.config.js
@@ -1,3 +1,11 @@
 module.exports = {
   target: "serverless",
+  functions: {
+    "pages/index.js": {
+      includeFiles: "posts/**/*.md",
+    },
+    "pages/posts/[id].js": {
+      includeFiles: "posts/**/*.md",
+    },
+  },
 };
